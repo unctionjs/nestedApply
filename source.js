0fda5489a8bbb9cd0a05e6ea8a2365873adcd32c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-07 14:39:20 -0700

    Copying over

--- source.js
+++ source.js
@@ -1,3 +1,29 @@
-export default function tempLate (alpha: mixed): mixed {
-  return alpha
+import reduceValues from "@unction/reducevalues"
+import upTo from "@unction/upto"
+import isPopulated from "@unction/ispopulated"
+
+export default function nestedApply (iterator: (any => any) => IterableType => IterableType): Function {
+  return function nestedApplyIterator (unction: any => any): Function {
+    const initial = iterator(unction)
+
+    return function nestedApplyIteratorUnction (depth: number): Function {
+      const times = upTo(depth)
+
+      if (isPopulated) {
+        return reduceValues(
+          function nestedApplyIteratorUnctionDepthIterable (accumulatedUnction: Function): Function {
+            return function nestedApplyIteratorUnctionDepthIterableAccumulatedUnction (): IterableType => IterableType {
+              return iterator(accumulatedUnction)
+            }
+          }
+        )(
+          initial
+        )(
+          times
+        )
+      }
+
+      return unction
+    }
+  }
 }
