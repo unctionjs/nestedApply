5c8b1442ea6bba929ffe92d699f85cbe0436cd64
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-27 01:45:00 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-import reduceValues from "@unction/reducevalues"
-import upTo from "@unction/upto"
-
-import type {UnaryFunctionType} from "types"
-import type {MapFunctionType} from "types"
-import type {EnumerableType} from "types"
-
-type IteratorFunctionType = MapFunctionType => EnumerableType => EnumerableType;
-
-export default function nestedApply (iterator: IteratorFunctionType): UnaryFunctionType {
-  return function nestedApplyIterator (unction: MapFunctionType): UnaryFunctionType {
-    return function nestedApplyIteratorUnction (depth: number): UnaryFunctionType {
-      return reduceValues(
-        (accumulatedUnction: IteratorFunctionType): UnaryFunctionType =>
-          (): IteratorFunctionType =>
-            iterator(accumulatedUnction)
-      )(
-        iterator(unction)
-      )(
-        upTo(depth)
-      )
-    }
-  }
+import reduceValues from "@unction/reducevalues";
+import upTo from "@unction/upto";
+export default function nestedApply(iterator) {
+  return function nestedApplyIterator(unction) {
+    return function nestedApplyIteratorUnction(depth) {
+      return reduceValues(accumulatedUnction => () => iterator(accumulatedUnction))(iterator(unction))(upTo(depth));
+    };
+  };
 }
